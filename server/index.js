4fd5522c07c29ddac51528011d77e87a61cdc22b
Nandika Yagnik <nandikayagnik@gmail.com>
2026-03-15 16:43:58 +0530

    feat connecting database and auth to main server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Study Planner API is running!" });
